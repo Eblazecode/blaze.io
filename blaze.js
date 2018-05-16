951547d5a5530778d6d150571467ebb3630b0231
ENOCH DANIEL <dynablaze@gmail.com>
2018-05-16 01:48:42 +0100

    new adjustment
    worked on the responsiveness and font

---
+++ blaze.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+});
+
+
+$('.togglebut').click(function() {
+    $('#viewans1').toggle('slow');
+});
+
+$('.togglebut2').click(function() {
+    $('#viewans2').toggle('slow');
+});
+
+$('.togglebut3').click(function() {
+    $('#viewans3').toggle('slow');
+});
+
+$('.togglebut4').click(function() {
+    $('#viewans4').toggle('slow');
+});
